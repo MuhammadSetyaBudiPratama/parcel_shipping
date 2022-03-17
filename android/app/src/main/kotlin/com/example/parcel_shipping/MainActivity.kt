edb48cddc2ea7eeeae7a49ec082bfdeb7dbebed2
MuhammadSetyaBudiPratama <msetyapratama27@gmail.com>
2022-03-17 08:10:50 +0700

    parcel app

---
+++ android/app/src/main/kotlin/com/example/parcel_shipping/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.parcel_shipping
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:parcel_shipping/app.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+// 
